ab2fd7e13b0ae54becd576f75677b467f9d9be53
Nick-Nordale <nicknordale@gmail.com>
2016-05-11 15:22:15 -0700

    dljflksaj

--- simple-calc/ViewController.swift
+++ simple-calc/ViewController.swift
@@ -20,12 +20,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    var rpn = false;
+    
     var op = ""
     var num = ""
-    var intArr = [Int]()
+    var numArr = [Double]()
     var newEq = true
 
     @IBOutlet weak var ans: UILabel!
+    
+    @IBAction func toggleRPN(sender: UIButton) {
+        rpn = !rpn
+        op = ""
+        num = ""
+        numArr.removeAll()
+        newEq = true
+        if rpn {
+            ans.text = "RPN is on"
+        } else {
+            ans.text = "RPN is off"
+        }
+    }
+    
+    @IBAction func enterNumber(sender: UIButton) {
+        if rpn {
+            if num != "" {
+                numArr.append(Double(num)!)
+                ans.text = num + " entered"
+                num = ""
+            }
+            
+        } else {
+            ans.text = "ERROR: RPN is off"
+        }
+    }
 
     @IBAction func numberButton(sender: UIButton) {
         if newEq {
@@ -40,25 +68,31 @@ class ViewController: UIViewController {
         if op != "" && op != sender.currentTitle! {
             ans.text = "ERROR: mismatch operations"
         } else {
-            intArr.append(Int(num)!)
+            if num != "" {
+                numArr.append(Double(num)!)
+            }
             num = ""
             ans.text = sender.currentTitle!
             op = sender.currentTitle!
+        }
+        
+        if rpn {
+            eq(sender)
         }
     }
     
     @IBAction func eq(sender: UIButton) {
         if num != "" {
-            intArr.append(Int(num)!)
+            numArr.append(Double(num)!)
         }
         if sender.currentTitle == "fact" {
-            if intArr.count > 1 {
+            if numArr.count > 1 {
                 ans.text = "ERROR: fact only accepts one number"
-            } else if intArr[0] > 20 {
+            } else if numArr[0] > 20 {
                 ans.text = "ERROR: answer too large"
             } else {
-                var out = 1
-                var i = intArr[0]
+                var out = 1.0
+                var i = numArr[0]
                 while i > 1 {
                     out = out * i
                     i -= 1
@@ -68,19 +102,39 @@ class ViewController: UIViewController {
         } else {
             switch op {
             case "+":
-                ans.text = "\(intArr[0] + intArr[1])"
+                if numArr.count != 2 {
+                    ans.text = "ERROR: (+) operator takes 2 numbers"
+                } else {
+                    ans.text = "\(numArr[0] + numArr[1])"
+                }
             case "-":
-                ans.text = "\(intArr[0] - intArr[1])"
+                if numArr.count != 2 {
+                    ans.text = "ERROR: (-) operator takes 2 numbers"
+                } else {
+                    ans.text = "\(numArr[0] - numArr[1])"
+                }
             case "*":
-                ans.text = "\(intArr[0] * intArr[1])"
+                if numArr.count != 2 {
+                    ans.text = "ERROR: (*) operator takes 2 numbers"
+                } else {
+                    ans.text = "\(numArr[0] * numArr[1])"
+                }
             case "/":
-                ans.text = "\(intArr[0] / intArr[1])"
+                if numArr.count != 2 {
+                    ans.text = "ERROR: (/) operator takes 2 numbers"
+                } else {
+                    ans.text = "\(numArr[0] / numArr[1])"
+                }
             case "%":
-                ans.text = "\(intArr[0] % intArr[1])"
+                if numArr.count != 2 {
+                    ans.text = "ERROR: (%) operator takes 2 numbers"
+                } else {
+                    ans.text = "\(numArr[0] % numArr[1])"
+                }
             case "count":
-                ans.text = "\(intArr.count)"
+                ans.text = "\(numArr.count)"
             case "avg":
-                ans.text = "\(intArr.reduce(0, combine: +)/intArr.count)"
+                ans.text = "\(numArr.reduce(0, combine: +)/(Double(numArr.count)))"
             default:
                 ans.text = "error"
             }
@@ -88,13 +142,13 @@ class ViewController: UIViewController {
         newEq = true
         num = ans.text!
         op = ""
-        intArr.removeAll()
+        numArr.removeAll()
     }
     
     @IBAction func clearButton(sender: UIButton) {
         op = ""
         num = ""
-        intArr.removeAll()
+        numArr.removeAll()
         newEq = true
         ans.text = "0"
     }
